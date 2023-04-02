9cdae3e520a9aaa6b538fd4aab92708405893093
Small <Small@s2time.com>
2023-04-03 02:29:52 +0800

    fix sequence exec multiple

--- plugin/executable/sequence/sequence_test.go
+++ plugin/executable/sequence/sequence_test.go
@@ -1,40 +1,40 @@
-// /*
-//  * Copyright (C) 2020-2022, IrineSistiana
-//  *
-//  * This file is part of mosdns.
-//  *
-//  * mosdns is free software: you can redistribute it and/or modify
-//  * it under the terms of the GNU General Public License as published by
-//  * the Free Software Foundation, either version 3 of the License, or
-//  * (at your option) any later version.
-//  *
-//  * mosdns is distributed in the hope that it will be useful,
-//  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  * GNU General Public License for more details.
-//  *
-//  * You should have received a copy of the GNU General Public License
-//  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//  */
+/*
+ * Copyright (C) 2020-2022, IrineSistiana
+ *
+ * This file is part of mosdns.
+ *
+ * mosdns is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mosdns is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-// package sequence
+package sequence
 
-// import (
-// 	"context"
-// 	"errors"
-// 	"github.com/IrineSistiana/mosdns/v5/coremain"
-// 	"github.com/IrineSistiana/mosdns/v5/pkg/query_context"
-// 	"github.com/miekg/dns"
-// 	"testing"
-// )
+import (
+	"context"
+	"errors"
+	"github.com/IrineSistiana/mosdns/v5/coremain"
+	"github.com/IrineSistiana/mosdns/v5/pkg/query_context"
+	"github.com/miekg/dns"
+	"testing"
+)
 
-// type dummy struct {
-// 	matched    bool
-// 	wantErr    error
-// 	wantR      *dns.Msg
-// 	dropR      bool
-// 	wantReturn bool
-// }
+type dummy struct {
+	matched    bool
+	wantErr    error
+	wantR      *dns.Msg
+	dropR      bool
+	wantReturn bool
+}
 
 // func (d *dummy) Match(ctx context.Context, qCtx *query_context.Context) (bool, error) {
 // 	if d.wantErr != nil {
